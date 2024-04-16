bcf1f281da77974668b8308233d41d7bfd1d6e90
Gustavo Nascimento <o_fantasma@hotmail.com.br>
2024-04-16 15:44:44 -0300

    Removido os dados sensíveis

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
-  subscription_id = "12106da5-bae4-4684-a905-de0669e308ee"
-  client_id       = "19d61ae5-e899-4007-b8f6-25542a053bb7"
-  client_secret   = "YJ78Q~fGAn52FNtI~b5yr4xOB2RucJd26-GB3bAE"
-  tenant_id       = "231800d7-f393-4413-a579-d72a2f586b33"
+  subscription_id = "#########################"
+  client_id       = "#########################"
+  client_secret   = "#########################"
+  tenant_id       = "#########################"
 }
